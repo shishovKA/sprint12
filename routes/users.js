62e189f9b83c06061fcfd85465b08332ae47044d
Konstantin Shishov <shishov.k.a@yandex.com>
2020-08-06 19:05:31 +0300

    structure add 2

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,17 @@
 const users = require('express').Router();
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-const loadFile = (req, res, next) => {
-  fs.readFile(path.join(__dirname, '../data/users.json'), (err, data) => { // передали вторым аргументом объект опций. Он содержит свойство encoding, куда записывают кодировку данных
-    if (err) {
+const loadFile = (req, res) => {
+  fsPromises.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(() => {
       console.log('файл не прочитался ', path.join(__dirname, 'data/users.json'));
-      return;
-    }
-    console.log('data: ', data.toString('utf8')); // здесь мы не вызываем метод toString, поскольку данные уже пришли в виде строки
-  });
-  next();
+    });
 };
 
 users.use(loadFile);
-
-users.get('/', (req, res) => {
-  res.send({ message: 'Вы запросили адрес /users' });
-});
 
 module.exports = users;
