9e5fad326cd1eed0e470fe41a9f952fbfd1ff336
Konstantin Shishov <shishov.k.a@yandex.ru>
2020-08-06 20:14:01 +0300

    card file read

--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,18 @@
 const cards = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-cards.get('/cards', (req, res) => {
-  res.send({ message: 'Вы запросили адрес /cards' });
-});
+const loadFile = (req, res) => {
+  const fileDir = path.join(__dirname, '../data/cards.json');
+  fsPromises.readFile(fileDir, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(() => {
+      res.status(500).send({ message: `Ошибка при загрузке файла по адресу ${fileDir}` });
+    });
+};
+
+cards.use(loadFile);
 
 module.exports = cards;
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,13 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 const loadFile = (req, res) => {
-  fsPromises.readFile(path.join(__dirname, '../data/users.json'), { encoding: 'utf8' })
+  const fileDir = path.join(__dirname, '../data/users.json');
+  fsPromises.readFile(fileDir, { encoding: 'utf8' })
     .then((data) => {
       res.send(data);
     })
     .catch(() => {
-      console.log('файл не прочитался ', path.join(__dirname, 'data/users.json'));
+      res.status(500).send({ message: `Ошибка при загрузке файла по адресу ${fileDir}` });
     });
 };
 
